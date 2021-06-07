7bedaf1a99dd3d4f7440df4da431040d0fad0057
Maneedhar <maneedhar@ideatostartup.org>
2021-06-07 23:23:17 +0530

    most starred languages

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -6,46 +6,58 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
   const { repos } = useContext(GithubContext);
 
-  let languages = repos.reduce((total, item) => {
-    const { language } = item;
+  const languages = repos.reduce((total, item) => {
+    const { language, stargazers_count } = item;
+
     if (!language) return total;
     if (!total[language]) {
-      total[language] = { label: language, value: 1 };
+      total[language] = { label: language, value: 1, stars: stargazers_count };
     } else {
       total[language] = {
         ...total[language],
         value: total[language].value + 1,
+        stars: total[language].stars + stargazers_count,
       };
     }
     return total;
   }, {});
 
-  languages = Object.values(languages)
+  const mostUsed = Object.values(languages)
     .sort((prev, next) => {
       return next.value - prev.value;
     })
     .slice(0, 5);
 
-  // const chartData = [
-  //   {
-  //     label: 'HTML',
-  //     value: '20',
-  //   },
-  //   {
-  //     label: 'CSS',
-  //     value: '23',
-  //   },
-  //   {
-  //     label: 'JavaScript',
-  //     value: '76',
-  //   },
-  // ];
+  const mostStarred = Object.values(languages)
+    .sort((prev, next) => {
+      return next.stars - prev.stars;
+    })
+    .map((item) => {
+      return { ...item, value: item.stars };
+    })
+    .slice(0, 5);
+
+  const chartData = [
+    {
+      label: 'HTML',
+      value: '20',
+    },
+    {
+      label: 'CSS',
+      value: '23',
+    },
+    {
+      label: 'JavaScript',
+      value: '76',
+    },
+  ];
 
   return (
     <section className="section">
       <Wrapper className="section-center">
-        {/* <ExampleChart data={chartData} /> */}
-        <Pie3D data={languages} />
+        <Pie3D data={mostUsed} />
+        <div></div>
+        <Doughnut2D data={mostStarred} />
       </Wrapper>
     </section>
   );
