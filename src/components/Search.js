51ca86c0138b023eca7d8b4dee68a41a78015a97
Maneedhar <maneedhar@ideatostartup.org>
2021-06-08 20:46:21 +0530

    search and number of requests

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
 
 const Search = () => {
   const [user, setUser] = useState('');
+  const { requests, error } = useContext(GithubContext);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -17,6 +18,11 @@ const Search = () => {
   return (
     <section className="section">
       <Wrapper className="section-center">
+        {error.show && (
+          <ErrorWrapper>
+            <p>{error.msg}</p>
+          </ErrorWrapper>
+        )}
         <form onSubmit={handleSubmit}>
           <div className="form-control">
             <MdSearch />
@@ -26,10 +32,10 @@ const Search = () => {
               value={user}
               onChange={(e) => setUser(e.target.value)}
             />
-            <button type="submit">Search</button>
+            {requests > 0 && <button type="submit">Search</button>}
           </div>
         </form>
-        <h3>Requests: 60 / 60</h3>
+        <h3>Requests: {requests} / 60</h3>
       </Wrapper>
     </section>
   );
--- src/context/context.js
+++ src/context/context.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect, createContext } from 'react';
 import mockUser from './mockData.js/mockUser';
 import mockRepos from './mockData.js/mockRepos';
 import mockFollowers from './mockData.js/mockFollowers';
-// import axios from 'axios';
+import axios from 'axios';
 
-// const rootUrl = 'https://api.github.com';
+const rootUrl = 'https://api.github.com';
 
 export const GithubContext = createContext();
 
@@ -13,8 +13,41 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  const [requests, setRequests] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const checkRequests = () => {
+    axios
+      .get(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        // console.log(data);
+        let {
+          rate: { remaining },
+        } = data;
+
+        setRequests(remaining);
+
+        if (remaining === 0) {
+          toggleErrors(true, 'Sorry you have exceeded your hourly rate limit');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const toggleErrors = (show = false, msg = '') => {
+    // console.log('toggleErrors ~ msg', msg);
+    setError({ show, msg });
+  };
+
+  useEffect(checkRequests, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
